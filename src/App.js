11c5e9f14727877843503728ceafef23424bee79
betomoedano <betomoedano@outlook.com>
2021-08-16 14:32:43 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import CreateUser from "./components/CreateUser"
+import {useEffect, useState} from "react";
 
 function App() {
+
+  const [profiles, setProfiles] = useState([]); 
+  const [newProfilePicture, setNewProfilePicture] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    return  await axios.get("http://localhost:3001/read")
+    .then((res) => {
+      //console.log(res.data);
+      setProfiles(res.data);
+    })
+    .catch((e) => {console.log(e)});
+  }
+
+  const updatePicture = async (id) => {
+    axios.put("http://localhost:3001/update", {
+      id: id,
+      newProfilePicture: newProfilePicture,
+    });
+  };
+
+  const deleteUser = async (id) => {
+    axios.delete(`http://localhost:3001/delete/${id}`);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <p>this is a MERN test</p>
+      <CreateUser/>
+      {
+        profiles.map((user, userID) => (
+          <div key={userID}>
+            <p>{user.firstName}</p>
+            <img src={user.profilePicture} style={{height:30, width:30}} alt=""/>
+            <input type="text" placeholder="Paste new picture link" onChange={(event) => {setNewProfilePicture(event.target.value)}}/>
+            <button onClick={() => updatePicture(user._id)}>Update</button>
+            <button onClick={() => deleteUser(user._id)}>Delete User</button>
+          </div>
+        ))
+      }
     </div>
   );
 }
---
+++ src/components/CreateUser.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useState } from "react"
+
+export default function CreateUser() {
+    
+    const [firstName, setFisrtName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [profilePicture, setProfilePicture] = useState("");
+    const [status, setStatus] = useState("");
+    const [email, setEmail] = useState("");
+
+    const saveUser = async () => {
+        await axios.post("http://localhost:3001/post",
+        {
+            firstName: firstName,
+            lastName: lastName,
+            profilePicture: profilePicture,
+            status: status,
+            email: email
+        }
+        );
+    }
+    return(
+        <form>
+            <h2>Create User</h2>
+            <label>First Name</label><br/>
+            <input placeholder="First Name" onChange={(event) => {setFisrtName(event.target.value)}}></input><br/>
+            <label>Last Name</label><br/>
+            <input placeholder="Last Name" onChange={(event) => {setLastName(event.target.value)}}></input><br/>
+            <label>Profile Picture</label><br/>
+            <input placeholder="Sting only" onChange={(event) => {setProfilePicture(event.target.value)}}></input><br/>
+            <label>Status</label><br/>
+            <input placeholder="Status" onChange={(event) => {setStatus(event.target.value)}}></input><br/>
+            <label>Email</label><br/>
+            <input placeholder="Email" onChange={(event) => {setEmail(event.target.value)}}></input><br/><br/>
+            <button onClick={saveUser}>Save</button>
+        </form>
+    )
+}
